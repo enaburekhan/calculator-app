455a37edab53f1df491a1abf590a4c093cf919d3
enaburekhan <ericenaburekhan2018@gmail.com>
2021-03-11 14:48:28 +0100

    create display component function

---
+++ src/components/Display.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+const Display = ({ value }) => (
+  <div className="component-display">
+    {value}  
+  </div>
+);
+
+Display.propTypes = {
+  value: PropTypes.string,
+};
+
+Display.defaultProps = {
+  value: '0',
+};
+
+export default Display;
