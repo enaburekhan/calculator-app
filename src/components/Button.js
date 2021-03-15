5e17d117774fb6c04546111645e165da9c1670dc
enaburekhan <ericenaburekhan2018@gmail.com>
2021-03-15 16:30:06 +0100

    refactor button function and update readme

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,24 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ clickHandler, name }) => {
+const Button = ({ name, clickHandler }) => {
   const handleClick = () => clickHandler(name);
-
+  const fetchResult = name => {
+    switch (name) {
+      case '0':
+        return 'Button zero';
+      case '+':
+      case '-':
+      case 'x':
+      case '/':
+      case '=':
+        return 'Button operator';
+      default:
+        return 'Button';
+    }
+  };
   return (
 
-    <button type="button" onClick={handleClick}>
+    <button type="button" onClick={handleClick} className={fetchResult(name)}>
       {name}
     </button>
 
