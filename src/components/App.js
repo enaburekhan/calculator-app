d8f6249b70210bb77189bb6256d9ec93eb437a6c
enaburekhan <ericenaburekhan2018@gmail.com>
2021-03-15 11:29:50 +0100

    make App function a class and initialize its constructor

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,31 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="Component-App">
-      <>
-        <Display />
-        <ButtonPanel />
-        <calculate />
-      </>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operate: null,
+    }
+  }
+
+
+  render () {
+    return (
+      <div className="Component-App">
+        <>
+          <Display />
+          <ButtonPanel />
+          
+        </>
+      </div>
+    );
+
+  }
 }
 
 export default App;
